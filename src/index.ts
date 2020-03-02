2a4adf1e80f79f4bad02694568d2ecf6d106bdbd
Chris Milson <chris.w.milson@gmail.com>
2020-03-02 11:35:31 +0900

    Add arguments to command
    The generate command will now take a master picture (path or url) from
    the cli, and will also take a config file, and an output location.

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,17 @@ import commander from 'commander'
 
 const program = new commander.Command(process.env.npm_package_name)
   .version(process.env.npm_package_version || 'Try running through yarn or npm')
-  .description('')
+  .description(process.env.npm_package_description || '')
 
 program
   .command('generate')
   .alias('g')
   .description('Generate icon assets from a master icon.')
-  .option('-c --config', 'Specify a config file for the icon generation.')
-  .option('-o --output', 'Specity an output directory for the assets.')
-  .action(generate)
+  .arguments('[master]')
+  .option('-c, --config <path>', 'Specify a config file for the icon generation.')
+  .option('-o, --output <path>', 'Specity an output directory for the assets.')
+  .action(cmd => {
+    generate(cmd || {}).catch(console.error)
+  })
 
 program.parse(process.argv)
